a1eaf53734ed2f7c8e08c2980d7f6a791ebaa1f8
Shifuuu31 <asaadane@student.1337.ma>
2024-11-19 19:21:05 +0100

    handle port

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
 	"sync"
 
 	"TCPChat/internal/utils"
@@ -15,10 +16,30 @@ var (
 	Messages   = []string{}
 	Mutex      = &sync.Mutex{}
 	MaxClients = 10
+	portHelp   = `
+Allowed Ports
+-Privileged Ports (0–1023):
+
+	-> Typically require root/administrator permissions to bind.
+	-> Reserved for well-known services (e.g., HTTP on 80, HTTPS on 443, SSH on 22).
+	-> Avoid unless your application is implementing a standard service.
+
+
+-Registered Ports (1024–49151):
+
+	-> Suitable for most server applications.
+	-> These ports are not restricted and are widely used for custom applications (e.g., 8080 for web development, 3306 for MySQL).
+
+
+-Dynamic/Private Ports (49152–65535):
+
+	-> Primarily used for client-side ephemeral connections.
+	-> Servers generally avoid binding to these ports unless required by specific use cases.
+`
 )
 
 func Run() {
-	listener := StartupServer()
+	listener := startupServer()
 	for {
 		clientConn, err := listener.Accept()
 		if err != nil {
@@ -36,16 +57,26 @@ func Run() {
 	}
 }
 
-func StartupServer() net.Listener {
+func getPort() string {
+	if len(os.Args) == 2 {
+		pt, err := strconv.Atoi(os.Args[1])
+		if err != nil || pt < 1024 || pt > 49151 {
+			fmt.Println("Invalid port")
+			fmt.Println(portHelp)
+			os.Exit(0)
+		}
+		return os.Args[1]
+	}
+	return "8989"
+}
+
+func startupServer() net.Listener {
 	if len(os.Args) > 2 {
 		fmt.Println("[USAGE]: ./TCPChat $port")
 		os.Exit(1)
 	}
 
-	port := "8989"
-	if len(os.Args) == 2 {
-		port = os.Args[1]
-	}
+	port := getPort()
 
 	listener, err := net.Listen("tcp", "localhost:"+port)
 	if err != nil {
--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -1,11 +1,29 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 const (
 	WelcomeMsg = "Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'"
 )
 
+var (
+	colors     = []string{"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m", "\033[37m"}
+	colorMutex = &sync.Mutex{}
+)
+
+func AssignColor(input string, nclients int) string {
+	colorMutex.Lock()
+	defer colorMutex.Unlock()
+
+	if nclients+1 > len(colors) {
+		return input
+	}
+	return fmt.Sprintf("%s%s\033[0m", colors[nclients], input)
+}
+
 func PrintBanner(port string) {
-	fmt.Printf("\033[1;32m\nTCPChat SERVER STATUS: 🟢 LIVE\n-> Server started on: localhost:%s\n-> Listening on the port: %s\033[0m\n", port, port)
+	fmt.Printf("\033[1;32m\nTCPChat SERVER STATUS: 🟢 LIVE\n-> Server started on: localhost: %s\n-> Listening on the port: %s\033[0m\n", port, port)
 }
